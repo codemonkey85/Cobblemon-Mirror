3b302d677757611fade11bb9dd0607a8b0a074a8
apion <apion@apionservers.com>
2024-08-23 23:14:40 -0400

    Add referential integrity

--- common/src/main/kotlin/com/cobblemon/mod/common/api/pokedex/Dexes.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/pokedex/Dexes.kt
@@ -9,6 +9,8 @@
 package com.cobblemon.mod.common.api.pokedex
 
 import com.cobblemon.mod.common.api.data.JsonDataRegistry
+import com.cobblemon.mod.common.api.pokedex.entry.DexEntries
+import com.cobblemon.mod.common.api.pokedex.entry.PokedexEntry
 import com.cobblemon.mod.common.api.reactive.SimpleObservable
 import com.cobblemon.mod.common.net.messages.client.data.DexDefSyncPacket
 import com.cobblemon.mod.common.util.adapters.IdentifierAdapter
@@ -23,6 +25,7 @@ import net.minecraft.server.packs.PackType
 object Dexes : JsonDataRegistry<PokedexDef> {
     override val id = cobblemonResource("dexes")
     override val type = PackType.SERVER_DATA
+    override val observable = SimpleObservable<Dexes>()
 
     override val gson: Gson = GsonBuilder()
         .registerTypeAdapter(ResourceLocation::class.java, IdentifierAdapter)
@@ -33,17 +36,24 @@ object Dexes : JsonDataRegistry<PokedexDef> {
     override val typeToken: TypeToken<PokedexDef> = TypeToken.get(PokedexDef::class.java)
     override val resourcePath = "dexes"
 
-    val entries = mutableMapOf<ResourceLocation, PokedexDef>()
+    //Maps a dex id to its PokedexDef
+    val dexEntryMap = mutableMapOf<ResourceLocation, PokedexDef>()
+    //Map dex id to map of pokedex entry id -> entry map
+    val entries = mutableMapOf<ResourceLocation, Map<ResourceLocation, PokedexEntry>>()
 
     override fun reload(data: Map<ResourceLocation, PokedexDef>) {
-        data.forEach { id, def ->
-            entries[id] = def
+        data.forEach { (location, entry) ->
+            val entryMap = entry.entries
+                .map { DexEntries.entries[it] ?: throw IllegalArgumentException("Unknown dex entry $it") }
+                .associateBy { it.id }
+            entries[location] = entryMap
         }
-        entries.putAll(data)
+        dexEntryMap.putAll(data)
     }
 
-    override val observable = SimpleObservable<Dexes>()
+    fun getDexEntryById(dexId: ResourceLocation, entryId: ResourceLocation) = entries[dexId]?.get(entryId)
+
     override fun sync(player: ServerPlayer) {
-        DexDefSyncPacket(entries.values)
+        DexDefSyncPacket(dexEntryMap.values)
     }
 }
