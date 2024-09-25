a966f0f9a638a6ddcd85d495e16bbd145cf07633
Polymeta <40173118+Polymeta@users.noreply.github.com>
2024-09-25 23:30:18 +0100

    dont reset state with empty state causing the loss of aspects
    Signed-off-by: Polymeta <40173118+Polymeta@users.noreply.github.com>

--- common/src/main/kotlin/com/cobblemon/mod/common/client/net/battle/BattleSwitchPokemonHandler.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/net/battle/BattleSwitchPokemonHandler.kt
@@ -13,7 +13,6 @@ import com.cobblemon.mod.common.client.CobblemonClient
 import com.cobblemon.mod.common.client.battle.ClientBattlePokemon
 import com.cobblemon.mod.common.client.battle.animations.MoveTileOffscreenAnimation
 import com.cobblemon.mod.common.client.battle.animations.MoveTileOnscreenAnimation
-import com.cobblemon.mod.common.client.render.models.blockbench.FloatingState
 import com.cobblemon.mod.common.net.messages.client.battle.BattleSwitchPokemonPacket
 import net.minecraft.client.Minecraft
 
@@ -40,7 +39,6 @@ object BattleSwitchPokemonHandler : ClientNetworkPacketHandler<BattleSwitchPokem
                 statChanges = statChanges
             ).also {
                 it.actor = actor
-                it.state = FloatingState()
             }
         }
 
