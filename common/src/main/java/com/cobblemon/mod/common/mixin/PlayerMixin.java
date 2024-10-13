4dae06c56c6abeab7f3e325d9acf215525f57471
JPAKx4 <superpizzaman10@gmail.com>
2024-10-12 19:27:10 -0500

    Adding scanning for Shouldered Entities. not yet finished, but not too bad

--- common/src/main/java/com/cobblemon/mod/common/mixin/PlayerMixin.java
+++ common/src/main/java/com/cobblemon/mod/common/mixin/PlayerMixin.java
@@ -38,7 +38,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
