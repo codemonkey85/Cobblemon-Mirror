6280fd542058e0f54fc1ed0ac8d86bc4321b80e0
Landon Wills <landonjwdev@gmail.com>
2024-10-13 00:20:34 -0400

    Hello code-changes lurkers

--- common/src/main/java/com/cobblemon/mod/common/mixin/ItemInHandRendererMixin.java
+++ common/src/main/java/com/cobblemon/mod/common/mixin/ItemInHandRendererMixin.java
@@ -8,16 +8,21 @@
 
 package com.cobblemon.mod.common.mixin;
 
+import com.cobblemon.mod.common.Rollable;
 import com.cobblemon.mod.common.item.PokedexItem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemInHandRenderer.class)
@@ -26,6 +31,16 @@ public class ItemInHandRendererMixin {
     private void cobblemon$renderArmWithItem(AbstractClientPlayer abstractClientPlayer, float f, float g, InteractionHand interactionHand, float h, ItemStack itemStack, float i, PoseStack poseStack, MultiBufferSource multiBufferSource, int j, CallbackInfo ci) {
         if (abstractClientPlayer.isUsingItem() && abstractClientPlayer.getUseItem().getItem() instanceof PokedexItem) {
             ci.cancel();
+        }
+        else if (abstractClientPlayer instanceof Rollable rollable && rollable.shouldRoll()) {
+
+        }
+    }
+
+    @Redirect(method = "renderHandsWithItems", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lorg/joml/Quaternionf;)V", ordinal = 1))
+    private void cobblemon$renderHandswithItems(PoseStack instance, Quaternionf quaternion) {
+        if (!(Minecraft.getInstance().player instanceof Rollable rollable && rollable.shouldRoll())) {
+            instance.mulPose(quaternion);
         }
     }
 }
