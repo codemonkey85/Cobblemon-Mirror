b4e402c83d671a8df26552d8fbbdb97ea3dad941
Hiroku <paul_l_preston@hotmail.com>
2024-10-21 07:15:46 +0000

    Update NPCBrain.kt

--- common/src/main/kotlin/com/cobblemon/mod/common/entity/npc/NPCBrain.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/entity/npc/NPCBrain.kt
@@ -30,71 +30,55 @@ import net.minecraft.world.entity.schedule.Activity
 
 object NPCBrain {
     fun configure(npcEntity: NPCEntity, npcClass: NPCClass, brain: Brain<out NPCEntity>) {
-        val brainConfigurationContext = BrainConfigurationContext()
-        brainConfigurationContext.apply(npcEntity, npcClass.ai)
-        return
+        BrainConfigurationContext().apply(npcEntity, npcClass.ai) // This redundancy will get cleaned up later when the rest of the framework is done
 
-        brain.addActivity(
-            Activity.CORE, ImmutableList.of(
-                Pair.of(0, StayAfloatTask(0.8F)),
-                Pair.of(0, GetAngryAtAttackerTask.create()),
-                Pair.of(0, StopBeingAngryIfTargetDead.create())
-            ))
+        // brain.addActivity(
+        //     Activity.CORE, ImmutableList.of(
+        //         Pair.of(0, StayAfloatTask(0.8F)),
+        //         Pair.of(0, GetAngryAtAttackerTask.create()),
+        //         Pair.of(0, StopBeingAngryIfTargetDead.create())
+        //     ))
 
-        brain.addActivity(
-            Activity.IDLE, ImmutableList.of(
-            Pair.of(2, RunOne(
-                ImmutableList.of(
-                    Pair.of(LookAtTargetSink(45, 90), 2),
-                    Pair.of(SetEntityLookTarget.create(15F), 2),
-                    Pair.of(ChooseWanderTargetTask.create(horizontalRange = 10, verticalRange = 5, walkSpeed = 0.33F, completionRange = 1), 1)
-                )
-            )),
-            Pair.of(1, FollowWalkTargetTask()),
-            Pair.of(0, SwitchToBattleTask.create()),
-            Pair.of(1, AttackAngryAtTask.create()),
-            Pair.of(1, MoveToAttackTargetTask.create()),
-            Pair.of(1, MeleeAttackTask.create("2".asExpression(), "30".asExpression())),
-            Pair.of(1, HealUsingHealingMachineTask()),
-            Pair.of(1, GoToHealingMachineTask.create(
-                horizontalSearchRange = "14".asExpression(),
-                verticalSearchRange = "5".asExpression(),
-                speedMultiplier = "0.33".asExpression(),
-                completionRange = "1".asExpression()
-            ))
-        ))
-        brain.addActivity(
-            NPC_BATTLING, ImmutableList.of(
-            Pair.of(0, SwitchFromBattleTask.create(Activity.IDLE)),
-            Pair.of(1, LookAtTargetSink(45, 90)),
-            Pair.of(2, LookAtBattlingPokemonTask.create()),
-        ))
-        brain.addActivity(
-            CobblemonActivities.NPC_ACTION_EFFECT, ImmutableList.of(
-            Pair.of(0, SwitchFromActionEffectTask.create())
-        ))
-        brain.addActivity(
-            CobblemonActivities.NPC_CHATTING, ImmutableList.of(
-            Pair.of(0, ExitSpeakersActivityTask.create()),
-            Pair.of(0, LookAtSpeakerTask.create()),
-            Pair.of(0, LookAtTargetSink(45, 90))
-        ))
-        brain.setCoreActivities(setOf(Activity.CORE))
-        brain.setDefaultActivity(Activity.IDLE)
-        brain.useDefaultActivity()
-
-
-
-//        val brainConfigurationContext = BrainConfigurationContext()
-////        val brainStruct = createNPCBrainStruct(npcEntity, brain, brainConfigurationContext)
-//        // run some scripts at some point
-//
-//        // apply the brain
-//        brainConfigurationContext.activities.forEach {
-////            brain.addActivity(it.activity, ImmutableList.copyOf(it.tasks))
-//        }
-//        brain.setCoreActivities(brainConfigurationContext.coreActivities)
-//        brain.setDefaultActivity(brainConfigurationContext.defaultActivity)
-//        brain.schedule = brainConfigurationContext.schedule
+        // brain.addActivity(
+        //     Activity.IDLE, ImmutableList.of(
+        //     Pair.of(2, RunOne(
+        //         ImmutableList.of(
+        //             Pair.of(LookAtTargetSink(45, 90), 2),
+        //             Pair.of(SetEntityLookTarget.create(15F), 2),
+        //             Pair.of(ChooseWanderTargetTask.create(horizontalRange = 10, verticalRange = 5, walkSpeed = 0.33F, completionRange = 1), 1)
+        //         )
+        //     )),
+        //     Pair.of(1, FollowWalkTargetTask()),
+        //     Pair.of(0, SwitchToBattleTask.create()),
+        //     Pair.of(1, AttackAngryAtTask.create()),
+        //     Pair.of(1, MoveToAttackTargetTask.create()),
+        //     Pair.of(1, MeleeAttackTask.create("2".asExpression(), "30".asExpression())),
+        //     Pair.of(1, HealUsingHealingMachineTask()),
+        //     Pair.of(1, GoToHealingMachineTask.create(
+        //         horizontalSearchRange = "14".asExpression(),
+        //         verticalSearchRange = "5".asExpression(),
+        //         speedMultiplier = "0.33".asExpression(),
+        //         completionRange = "1".asExpression()
+        //     ))
+        // ))
+        // brain.addActivity(
+        //     NPC_BATTLING, ImmutableList.of(
+        //     Pair.of(0, SwitchFromBattleTask.create(Activity.IDLE)),
+        //     Pair.of(1, LookAtTargetSink(45, 90)),
+        //     Pair.of(2, LookAtBattlingPokemonTask.create()),
+        // ))
+        // brain.addActivity(
+        //     CobblemonActivities.NPC_ACTION_EFFECT, ImmutableList.of(
+        //     Pair.of(0, SwitchFromActionEffectTask.create())
+        // ))
+        // brain.addActivity(
+        //     CobblemonActivities.NPC_CHATTING, ImmutableList.of(
+        //     Pair.of(0, ExitSpeakersActivityTask.create()),
+        //     Pair.of(0, LookAtSpeakerTask.create()),
+        //     Pair.of(0, LookAtTargetSink(45, 90))
+        // ))
+        // brain.setCoreActivities(setOf(Activity.CORE))
+        // brain.setDefaultActivity(Activity.IDLE)
+        // brain.useDefaultActivity()
     }
 }
