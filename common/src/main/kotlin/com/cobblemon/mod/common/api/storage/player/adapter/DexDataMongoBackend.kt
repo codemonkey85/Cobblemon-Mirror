d568644155e643504348b3e0f88975321327be08
navneetset <navneetset@gmail.com>
2024-11-02 10:05:24 +0100

    Added dex manager adapter to JSON and MongoDB codec

--- common/src/main/kotlin/com/cobblemon/mod/common/api/storage/player/adapter/DexDataMongoBackend.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/storage/player/adapter/DexDataMongoBackend.kt
@@ -10,12 +10,10 @@ package com.cobblemon.mod.common.api.storage.player.adapter
 
 import com.cobblemon.mod.common.api.pokedex.PokedexManager
 import com.cobblemon.mod.common.api.storage.player.PlayerInstancedDataStoreTypes
-import com.cobblemon.mod.common.util.adapters.IdentifierAdapter
-import com.google.gson.Gson
+import com.cobblemon.mod.common.util.adapters.CodecBackedAdapter
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import com.mongodb.client.MongoClient
-import net.minecraft.resources.ResourceLocation
 import java.util.UUID
 
 /**
@@ -24,10 +22,11 @@ import java.util.UUID
  * @author Pebbles
  * @since October 26, 2024
  */
+
 class DexDataMongoBackend(mongoClient: MongoClient, databaseName: String, collectionName: String) :
     MongoBackedPlayerDataStoreBackend<PokedexManager>(mongoClient, databaseName, collectionName, PlayerInstancedDataStoreTypes.POKEDEX) {
     override val gson = GsonBuilder()
-        .registerTypeAdapter(ResourceLocation::class.java, IdentifierAdapter)
+        .registerTypeAdapter(PokedexManager::class.java, CodecBackedAdapter(PokedexManager.CODEC))
         .create()
     override val classToken = TypeToken.get(PokedexManager::class.java)
     override val defaultData = defaultDataFunc
